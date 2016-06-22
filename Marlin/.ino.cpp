d9f4c1412297cab5523fd6b5c3a87acbb7aa871b
fgramazio <gramazio@arch.ethz.ch>
2016-06-22 17:31:31 +0200

    Add files via upload

---
+++ Marlin/.ino.cpp
@@ -0,0 +1,22 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2016-06-21 16:21:02
+
+#include "Arduino.h"
+#include "Configuration.h"
+#include "pins.h"
+#include <LiquidCrystal_I2C.h>
+#include <LiquidTWI2.h>
+#include <U8glib.h>
+#include <LiquidCrystal.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Wire.h>
+#include <Wire.h>
+
+#include "Marlin.ino"
+
+
+#endif
